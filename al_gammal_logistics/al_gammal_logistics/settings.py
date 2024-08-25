0d24b37a4ec9c77501e8ac7e509911bfc4af1230
Ansary <abduallah.nadi.hassan@gmail.com>
2024-08-25 15:40:53 +0200

    added requirements.txt and changed db to sqlite3

--- al_gammal_logistics/al_gammal_logistics/settings.py
+++ al_gammal_logistics/al_gammal_logistics/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'al_gammal_logistics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
