5b54832151a36f83baadc0f44b8f134acc35448a
Ansary <abduallah.nadi.hassan@gmail.com>
2024-09-01 15:37:44 +0200

    registered Services model in the admin site

--- al_gammal_logistics/homepage/admin.py
+++ al_gammal_logistics/homepage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Article, Jobs
+from .models import Article, Jobs, Services
 
 admin.site.register(Article)
 admin.site.register(Jobs)
+admin.site.register(Services)
