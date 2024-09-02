2dff9f828c89282e24c9ff2028ff5342eb72f325
nadagmal <nadagamalsobhy@gmail.com>
2024-09-02 16:03:10 +0300

    update the empTxt2 and the partners

--- al_gammal_logistics/homepage/static/homepage/js/main.js
+++ al_gammal_logistics/homepage/static/homepage/js/main.js
@@ -49,10 +49,10 @@ window.onscroll = function() {
 };
 
 // spinner
-window.addEventListener("load", function() {
-    const loadingScreen = document.getElementById('loading-screen');
-    loadingScreen.style.display = 'none';
-});
+// window.addEventListener("load", function() {
+//     const loadingScreen = document.getElementById('loading-screen');
+//     loadingScreen.style.display = 'none';
+// });
 
 
 
