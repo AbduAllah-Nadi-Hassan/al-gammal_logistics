563da67921839acceebb076d13e34522cf1aa566
Ansary <abduallah.nadi.hassan@gmail.com>
2024-09-02 03:43:45 +0200

    modified admin site header and title

--- al_gammal_logistics/homepage/admin.py
+++ al_gammal_logistics/homepage/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Article, Jobs, Services
 
+admin.site.site_header = "Al-Gammal Admin"
+admin.site.site_title = "Al-Gammal Admin"
+
 admin.site.register(Article)
 admin.site.register(Jobs)
 admin.site.register(Services)
