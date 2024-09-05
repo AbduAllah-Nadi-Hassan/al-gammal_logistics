3b67bc70b426348608d1b714e39df99edc7e3be4
Ansary <abduallah.nadi.hassan@gmail.com>
2024-09-05 18:05:23 +0200

    populated the db with projects' data

--- al_gammal_logistics/homepage/admin.py
+++ al_gammal_logistics/homepage/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from .models import Article, Jobs, Services, Order, Fleet
+from .models import Article, Jobs, Services, Order, Fleet, Project
 
 admin.site.site_header = "Al-Gammal Admin"
 admin.site.site_title = "Al-Gammal Admin"
 
 admin.site.register(Article)
 admin.site.register(Jobs)
-admin.site.register(Services)
 admin.site.register(Order)
-admin.site.register(Fleet)
