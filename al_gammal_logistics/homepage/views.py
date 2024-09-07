cb4ec7a3528db123f2491323267fc25df83fb75f
Ansary <abduallah.nadi.hassan@gmail.com>
2024-09-07 16:26:10 +0200

    added who are we video and links to project and fleet

--- al_gammal_logistics/homepage/views.py
+++ al_gammal_logistics/homepage/views.py
@@ -70,9 +70,11 @@ class CreateOrder(CreateView):
 
 def fleet(request, fleet_id):
     fleet = get_object_or_404(Fleet, pk=fleet_id)
-    return render(request, "homepage/fleet.html", {"fleet": fleet})
+    other_fleets = Fleet.objects.exclude(pk=fleet_id)
+    return render(request, "homepage/fleet.html", {"fleet": fleet, "other_fleets": other_fleets})
 
 
 def project(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
-    return render(request, "homepage/project.html", {"project": project})
+    other_projects = Project.objects.exclude(pk=project_id)
+    return render(request, "homepage/project.html", {"project": project, "other_projects": other_projects})
